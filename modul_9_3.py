f17500576e2be4acda0a4f9fb234d066dd2ad18e
Igor-user1 <xadeev.igor@icloud.com>
2024-10-19 17:11:00 +0300

    Add files via upload

---
+++ modul_9_3.py
@@ -0,0 +1,10 @@
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+list_1 = list(zip(first, second))
+
+first_result = (len(x) - len(y) for x, y in list_1 if len(x) != len(y))
+print(list(first_result))
+
+second_result = (len(first[x]) == len(second[y]) for x in range(3) for y in range(3) if x == y)
+print(list(second_result))
